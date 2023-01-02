932d71a923cba0e67e009c7cc0475bdc1374979a
ahmedshukri20 <30628403+ahmedshukri20@users.noreply.github.com>
2023-01-02 11:29:13 +0100

    update subscribe section

--- components/bigdata-analytics/StartYourFreeTrial.vue
+++ components/bigdata-analytics/StartYourFreeTrial.vue
@@ -9,10 +9,10 @@
                 </div>
 
                 <div class="col-lg-6 col-md-12 p-0">
-                    <div class="newsletter-content">
-                        <h2>Subscribe to our newsletter</h2>
+                    <div class="newsletter-content p-2">
+                        <h2 class="text-dark">Subscribe to our newsletter</h2>
                         <form class="newsletter-form">
-                            <input type="email" class="input-newsletter" placeholder="Enter your business email here">
+                            <input type="email" class="input-newsletter" placeholder="Email Address">
                             <button type="submit">Subscribe</button>
                         </form>
                         <!-- <p>Test out the Big Data Analytics features for 14 days, no credit card required.</p> -->
